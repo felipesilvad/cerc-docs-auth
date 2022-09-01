0ae6128098a3a31e44cb88ea74e17021d794759e
felipesilvad <31076991+felipesilvad@users.noreply.github.com>
2022-09-01 17:44:12 -0300

    Icons & Header

--- src/components/Produto.js
+++ src/components/Produto.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Card, Button, Container, } from 'react-bootstrap';
+import { Card, Button, Container,Image } from 'react-bootstrap';
+import logo from '../imgs/LOGO_CERC.DOCS.png';
+import arranjo_de_pagamento from '../imgs/arranjo_de_pagamento.png';
+import imobiliario from '../imgs/imobiliario.png';
+import duplicata_escritural from '../imgs/duplicata_escritural.png';
 
 const Produto = () => {
   return (
-    <Container class="produto">
+    <Container>
       <header className='produto-head'>
-
+        <Image src={logo} className="p-4 logo-head d-block"/>
       </header>
-      <div>
+      <div class="produto">
         <div className='text-center p-4'>
-          <h3 className='produto-txt'>Escolha um Produto abaixo para acessar o cerc.docs:</h3>
+          <h3 className='produto-txt'>Escolha um produto abaixo para acessar o cerc.docs:</h3>
           <div className='d-flex justify-content-center mt-2 pt-2'>
             <p className='w-50 produto-sub-txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor vel purus a scelerisque.</p>
           </div>
@@ -18,7 +22,10 @@ const Produto = () => {
         <div className='card-container justify-content-center'>
           <Card body className='produto-card'>
             <Card.Body>
-              <h4 className='card-title'>Duplicata <br />Escritural</h4>
+              <div className='d-flex justify-content-center'>
+                <Image className='produto-icon' src={duplicata_escritural} />
+                <h4 className='card-title'>Duplicata <br />Escritural</h4>
+              </div>
               <ul className='card-ul'>
                 <li>Cras justo odio</li>
                 <li>Dapibus ac facilisis in</li>
@@ -33,7 +40,10 @@ const Produto = () => {
           </Card>
           <Card body className='produto-card'>
             <Card.Body>
-              <h4 className='card-title'>Imobiliário <br /><span className='invisible'>.</span></h4>
+              <div className='d-flex justify-content-center'>
+                <Image className='produto-icon' src={imobiliario} />
+                <h4 className='card-title mt-1 mb-4'>Imobiliário</h4>
+              </div>
               <ul className='card-ul'>
                 <li>Cras justo odio</li>
                 <li>Dapibus ac facilisis in</li>
@@ -48,7 +58,10 @@ const Produto = () => {
           </Card>
           <Card body className='produto-card'>
             <Card.Body>
-              <h4 className='card-title'>Arranjo de<br />Pagamento</h4>
+              <div className='d-flex justify-content-center'>
+                <Image className='produto-icon' src={arranjo_de_pagamento} />
+                <h4 className='card-title'>Arranjo de<br />Pagamento</h4>
+              </div>
               <ul className='card-ul'>
                 <li>Cras justo odio</li>
                 <li>Dapibus ac facilisis in</li>
